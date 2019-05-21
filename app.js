54909ebde700a59c256bba0128e2380cf43a88be
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-05-21 15:53:46 -0400

    send html file to reponse

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const data = require(__dirname + '/applications.json'); // retrieve JSON data from file
+const path = require('path');
 
+// set up creation of server
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Test');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(4000);
